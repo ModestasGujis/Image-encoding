4b780bee702426b3c8b9cf4cc22ff1c6d9e39111
ModestasGujis <gujismodestas@gmail.com>
2021-03-22 01:57:27 +0200

    support for RGB color mode

--- image_encoding.py
+++ image_encoding.py
@@ -15,32 +15,58 @@ def string2bits(s):
 
 def bits2string(binary_string):
 	bits = [int(binary_string[i - BIT_COUNT:i], 2) for i in range(BIT_COUNT, len(binary_string), BIT_COUNT)]
-	
+		
 	bits = bits[:bits.index((1 << BIT_COUNT) - 1)]
 	return bytes(bits).decode(ENCODING)
 
-def encode8bit(pixel, binary_string): # encode 8bit integer pixels
+def encode8(pixel, binary_string):
 	# remove the least significant encoding bits
 	pixel = pixel >> ENCODING_BIT_COUNT
 	pixel = pixel << ENCODING_BIT_COUNT
 
 	return pixel | int(binary_string[:ENCODING_BIT_COUNT], 2)
 
+def decode8(pixel):
+	b = ENCODING_BIT_COUNT
+	return (bin(pixel)[2:])[-b:].zfill(b)
+
+def encode3x8(pixel, binary_string):
+	pixel_list = list(pixel)
+
+	e = ENCODING_BIT_COUNT
+	for i in range(3):
+		if len(binary_string) >= ENCODING_BIT_COUNT:
+			pixel_list[i] = encode8(pixel_list[i], binary_string)
+			binary_string = binary_string[ENCODING_BIT_COUNT:]
+
+	return tuple(pixel_list)
+
+def decode3x8(pixel):
+	pixel_list = list(pixel)
+	b = ENCODING_BIT_COUNT
+
+	return ''.join([str((bin(px)[2:])[-b:].zfill(b)) for px in pixel_list])
+
 def decode(image_file):
 	im = Image.open(image_file)
 
 	(w, h) = im.size
 
-	if im.mode != "L":
-		print('only mode L is supported (for now) ;(')
+	decodePixel = 'temp'
+
+	if im.mode == 'L':
+		decodePixel = decode8
+	elif im.mode == 'RGB':
+		decodePixel = decode3x8
+	else:
+		print('only L and RGB modes are supported (for now) ;(')
 		sys.exit()
 
 	px = im.load()
 	binary_string = ''
 	for i in range(w):
 		for j in range(h):
-			b = ENCODING_BIT_COUNT
-			binary_string += (bin(px[i,j])[2:])[-b:].zfill(b)
+			binary_string += decodePixel(px[i,j])
 
 	text = bits2string(binary_string)
 	print(text)
@@ -55,7 +81,22 @@ def encode(text_file, image_file):
 	im = Image.open(image_file)
 	(w, h) = im.size
 
-	have_bits = w*h*ENCODING_BIT_COUNT
+	encoded_bits_per_pixel = 0
+	encodePixel = 3
+
+	if im.mode == 'L':
+		encodePixel = encode8
+		encoded_bits_per_pixel = ENCODING_BIT_COUNT
+	elif im.mode == 'RGB':
+		encodePixel = encode3x8
+		encoded_bits_per_pixel = 3 * ENCODING_BIT_COUNT
+	else:
+		im = im.convert('RGB')
+		encodePixel = encode3x8
+		encoded_bits_per_pixel = 3 * ENCODING_BIT_COUNT
+
+
+	have_bits = w*h*encoded_bits_per_pixel
 	need_bits = len(binary_string)
 
 	# for easier implementation we pad the binary string
@@ -66,22 +107,18 @@ def encode(text_file, image_file):
 		print('not enough space on the image ;(')
 		sys.exit()
 
-	# TODO implement other mode encodings
-	if im.mode != "L":
-		im = im.convert("L")
-
 	px = im.load()
 
 	for i in range(w):
 		for j in range(h):
 			if len(binary_string) <= 0:
 				break
-			px[i,j] = encode8bit(px[i,j], binary_string)
-			binary_string = binary_string[ENCODING_BIT_COUNT:]
-
-	new_file_name = 'encoded_' + image_file.rsplit(".", 1)[0] + '.png'
+			px[i,j] = encodePixel(px[i,j], binary_string)
+			binary_string = binary_string[encoded_bits_per_pixel:]
 
 	# save as png to preserve pixels and avoid compression
+	new_file_name = 'encoded_' + image_file.rsplit(".", 1)[0] + '.png'
+
 	print(new_file_name, 'has been generated')
 	im.save(new_file_name)
 
